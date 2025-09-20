574e44b294ee01d8ef820aa7cec95bab6b8df504
fominavasilisaa <fominavasilisaa@gmail.com>
2025-09-20 15:47:25 +0300

    hw.cpp

--- hw.cpp
+++ hw.cpp
@@ -16,3 +16,17 @@ struct Point {
 		cout << "y: " << y << endl;
 	}
 };
+
+int main() {
+	double x = 0, y = 0;
+	cout << "Введите x" << endl;
+	cin >> x;
+	cout << "Введите y" << endl;
+	cin >> y;
+
+	Point point;
+	point.setpoint(x, y);
+	point.pointPrint();
+
+	return 0;
+}
